35c77e172918f4426de46f4a9e9bce8f4623d40a
Kazuhiiro <64940750+Kazuhiiro@users.noreply.github.com>
2023-11-10 14:19:09 -0300

    Add files via upload

---
+++ Aplicação Teste/Aplicação Teste/Controllers/APIController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aplicação_Teste.Controllers
+{
+    [Route("API/[controller]")]
+    [ApiController]
+    public class Calculos : ControllerBase
+    {
+        [HttpGet("Somar")]
+        public String somar (Int16 num1, Int16 num2)
+        {
+            return (num1 + num2).ToString();
+        }
+
+        [HttpGet("Subrair")]
+        public String subtrair (Int16 num1, Int16 num2)
+        {
+            return (num1 - num2).ToString();
+        }
+    }
+}
